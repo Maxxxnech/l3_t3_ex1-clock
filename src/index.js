0c34d6261e6a75caf5c08727fcf1a4a6e4c8e364
Maxxxnech <51296105+Maxxxnech@users.noreply.github.com>
2022-01-02 20:53:02 +0300

    removed unused, added clocks

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Profiler } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function renderChecker(content, tgt) {
+  var text = "Рендер " + content + " в " + new Date().toLocaleTimeString();
+  console.log(text);
+  var target = document.querySelector(tgt);
+  if (target) {
+    target.textContent = text;
+    target.style.color = target.style.color == "green" ? "red" : "green";
+  }
+}
+
+function ticker(targetId, number) {
+  var headerString = "Текущее время, интервал " + number + "с";
+  const timeElement = (
+    <div className="dial">
+      <Profiler
+        id={"header_" + number}
+        onRender={() =>
+          renderChecker("заголовка ", ".logHeader_" + number)
+        }
+      >
+        <h2>{headerString}</h2>
+      </Profiler>
+
+      <Profiler
+        id={"time_" + number}
+        onRender={() =>
+          renderChecker("циферблата " + number, ".logTime_" + number)
+        }
+      >
+        <div>{new Date().toLocaleTimeString()}</div>
+      </Profiler>
+
+      <p>
+        <span className={"logHeader_" + number}></span>
+      </p>
+      <p>
+        <span className={"logTime_" + number}></span>
+      </p>
+    </div>
+  );
+  ReactDOM.render(timeElement, document.getElementById(targetId));
+}
+//first render at load
+let time1 = 30;
+let time2 = 1;
+ticker("root", time1);
+ticker("root_1", time2);
+
+var int = setInterval(() => ticker("root", time1), time1 * 1000);
+
+var int2 = setInterval(
+  () => ticker("root_1", time2),
+  time2 * 1000
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

4b6d1e4cdb7f3aec0a31f5ee9979e9faf5c2a5dc
Maxxxnech <51296105+Maxxxnech@users.noreply.github.com>
2022-01-03 14:02:18 +0300

    updated profiling

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React, { Profiler } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function renderChecker(content, tgt) {
-  var text = "Рендер " + content + " в " + new Date().toLocaleTimeString();
+/**
+ *
+ * @param {String} type - React element
+ * @param {String} tgt - onscreen log target
+ */
+function renderChecker(type, tgt, actualDuration, startTime) {
+  var text =
+    "Рендер " + type + ": " + actualDuration.toFixed(6) + " мс, timestamp начала рендеринга: " + startTime.toFixed(4) + "";
   console.log(text);
   var target = document.querySelector(tgt);
   if (target) {
@@ -12,14 +18,24 @@ function renderChecker(content, tgt) {
   }
 }
 
+/**
+ * Renders clock
+ * @param {String} targetId - DOM element to render into
+ * @param {Number} number - some unique  id, for ex.- clock update interval
+ */
 function ticker(targetId, number) {
   var headerString = "Текущее время, интервал " + number + "с";
   const timeElement = (
     <div className="dial">
       <Profiler
         id={"header_" + number}
-        onRender={() =>
-          renderChecker("заголовка для " + number + "c", ".logHeader_" + number)
+        onRender={(id, phase, actualDuration, baseDuration, startTime) =>
+          renderChecker(
+            "заголовка ",
+            ".logHeader_" + number,
+            actualDuration,
+            startTime
+          )
         }
       >
         <h2>{headerString}</h2>
@@ -27,32 +43,37 @@ function ticker(targetId, number) {
 
       <Profiler
         id={"time_" + number}
-        onRender={() =>
-          renderChecker("циферблата для " + number + "c", ".logTime_" + number)
+        onRender={(id, phase, actualDuration, baseDuration, startTime) =>
+          renderChecker(
+            "циферблата ",
+            ".logTime_" + number,
+            actualDuration,
+            startTime
+          )
         }
       >
         <div>{new Date().toLocaleTimeString()}</div>
       </Profiler>
 
-      <p>
-        <span className={"logHeader_" + number}></span>
-      </p>
-      <p>
-        <span className={"logTime_" + number}></span>
-      </p>
+      <ul className="log_output">
+        <li>
+          <span className={"logHeader_" + number}></span>
+        </li>
+        <li>
+          <span className={"logTime_" + number}></span>
+        </li>
+      </ul>
     </div>
   );
   ReactDOM.render(timeElement, document.getElementById(targetId));
 }
-//first render at load
+
 let time1 = 30;
 let time2 = 1;
+
+//first render at load
 ticker("root", time1);
 ticker("root_1", time2);
 
 var int = setInterval(() => ticker("root", time1), time1 * 1000);
-
-var int2 = setInterval(
-  () => ticker("root_1", time2),
-  time2 * 1000
-);
+var int2 = setInterval(() => ticker("root_1", time2), time2 * 1000);
